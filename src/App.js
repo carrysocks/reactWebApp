1be4acaffae3fc2411a0ba813462f35204da6e5e
carrysocks <39540655+carrysocks@users.noreply.github.com>
2020-03-21 23:58:52 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
-import "./styles.css";
 
-export default function App() {
+function Wallpaper({ name, picture }) {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h2>{name}</h2>
+      <img src={picture} alt={name} />
     </div>
   );
+}
+
+const WhoWallpaper = [
+  {
+    id: "1",
+    name: "joy",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQrSvP6xK9fRklE2ph3JB6FnQOHUd5RyVvFuCwqbDT9qo0inzFl"
+  }
+];
+
+function renderWho(who) {
+  return <Wallpaper key={who.id} name={who.name} picture={who.image} />;
+}
+
+export default function App() {
+  return <div>{WhoWallpaper.map(renderWho)}</div>;
 }
